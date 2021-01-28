ae896a681e96963ec2459117e42324dc0d8033c0
NicolasArayaB <nicolasaraya@gmail.com>
2021-01-28 15:50:58 +0000

    done

--- .breathecode/resets/06-add-listener-with-js/index.js
+++ .breathecode/resets/06-add-listener-with-js/index.js
@@ -1,10 +1,6 @@
 window.onload = function myLoadFunction() {
 	alert("The website just finished loading!");
 	//some code here
-	clickEvent();
 };
 
 //the listener function here
-const clickEvent = () => {
-	document.getElementById("theGreen").addEventListener("click", () => alert("wuju"));
-};
--- exercises/05-the-load-event/index.js
+++ exercises/05-the-load-event/index.js
@@ -1,1 +1,10 @@
 // your function goes here
+// Revisar, no logro encontrar el porque no carga el alert
+
+//window.loadListener = function loadListener() {
+//	document.body.addEventListener("load", () => alert("loading finished"));
+//};
+
+window.onload = function loadListener() {
+	alert("loading finished");
+};
